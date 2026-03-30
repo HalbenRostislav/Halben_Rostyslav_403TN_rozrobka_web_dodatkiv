3fb97282bc16d78cb692dacc660ecb89a7f60727
HalbenR <halbenrrr@gmail.com>
2026-03-30 04:52:10 +0300

    Очищення: залишено лише матеріали для Лабораторної №2

--- program/smartstock-server/src/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ProductController = require('../controllers/ProductController');
-const { protect, authorize } = require('../middleware/authMiddleware');
-router.get('/', ProductController.getAll);
-router.post('/', protect, authorize('ADMIN'), ProductController.create);
-router.patch('/:id/stock', protect, authorize('ADMIN', 'MANAGER'), ProductController.updateStock);
-module.exports = router;
